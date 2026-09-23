6b16d09f5e8e806c9c09cb3e77c69815c36331ef
torisar93 <mradzzz93@gmail.com>
2026-09-23 16:47:20 +0300

    release: 1.0.36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "ru.magicsqd.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 99
-        versionName = "1.0.35"
+        versionCode = 100
+        versionName = "1.0.36"
 
         ndk {
             // arm64-v8a/armeabi-v7a — реальные телефоны; x86_64 — эмулятор для UI-отладки
